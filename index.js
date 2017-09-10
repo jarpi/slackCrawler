5e6a697763ca13eaac77c981b25e9cfc963ae8db
Josep <josep.arpi@gmail.com>
2017-09-10 12:12:06 +0200

    Feature/Crawler - Slack accessLogs Generalize the use case of running requests - Create the basic flow to retrieve data from Slack and store it in independent files - Generalize the use case, to simply add a code file that retrieves from a source, and stores it in the file with the same name as code file with timestamp appended as json

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-var WebClient = require('@slack/client').WebClient;
-var fs = require('fs')
+const fs = require('fs')
+const Path = require('path')
+const collectorsDir = Path.resolve('.', 'lib/collectors')
 
+// GET ALL PAGES FROM RESPONSE
 
-var token = process.env.SLACK_API_TOKEN || '';
+const token = process.env.SLACK_API_TOKEN || '';
 
-var web = new WebClient(token);
-web.team.accessLogs((err, res) => {
-	if (err) return console.dir(err);
-	fs.writeFile('test.json', JSON.stringify(res), (err) => {
-		if (err) return console.dir(err)
-		console.dir('write ok')
+const getCollectors = (dir) => {
+	return new Promise((resolve, reject) => {
+		return fs.readdir(dir, (err, files) => {
+			if (err) return reject(err)
+			resolve(files)
+		})
 	})
-});
+}
+
+const writeFile = (data, fileName) => {
+	return new Promise((resolve, reject) => {
+        const fileLog = fileName + '.' + Date.now() + '.json';
+		return fs.writeFile(fileLog, JSON.stringify(data), (err) => {
+			if (err) return reject(err)
+			resolve(fileLog)
+		})
+	})
+}
+
+const printOKMessage = (fileName) => {
+	console.dir(`File written to disk: ${fileName}`)
+}
+
+const printKOMessage = (err) => {
+	console.dir(err)
+}
+
+getCollectors(collectorsDir)
+.then(files => {
+	files.forEach((collectorFile) => {
+		const collector = require(Path.resolve('./', collectorsDir, collectorFile))
+		collector(token)
+		.then((data) => {
+            const fileNameGroups = /([^\.]+)\./.exec(collectorFile)
+            if (!fileNameGroups) throw new Error(collectorFile + ' invalid file name')
+			return writeFile(data, fileNameGroups[1] || fileNameGroups[0])
+		})
+		.then(printOKMessage)
+		.catch(printKOMessage)
+
+	})
+})
+.catch((err) => {
+	console.dir(err)
+})
+
