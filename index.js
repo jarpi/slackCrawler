9a7c5d9151886ce4a4c6d3623fedc62736eff319
Josep <josep.arpi@gmail.com>
2017-09-13 20:40:51 +0200

    Feature Add cronjob to execute query every 5 minutes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const accessLogsCollector = require('./lib/collectors/slackTeamAccessLogs.js')
+const path = require('path')
 const fs = require('fs')
-const Path = require('path')
-const collectorsDir = Path.resolve('.', 'lib/collectors')
-
-// GET ALL PAGES FROM RESPONSE
+const cronJob = require('node-cron')
+const collectorsDir = path.resolve('.', 'lib/collectors')
 
 const token = process.env.SLACK_API_TOKEN || '';
+
 
 const getCollectors = (dir) => {
 	return new Promise((resolve, reject) => {
@@ -15,39 +16,31 @@ const getCollectors = (dir) => {
 	})
 }
 
-const writeFile = (data, fileName) => {
-	return new Promise((resolve, reject) => {
-        const fileLog = fileName + '.' + Date.now() + '.json';
-		return fs.writeFile(fileLog, JSON.stringify(data), (err) => {
-			if (err) return reject(err)
-			resolve(fileLog)
-		})
-	})
-}
-
 const printOKMessage = (fileName) => {
-	console.dir(`File written to disk: ${fileName}`)
+	console.dir('File updated')
 }
 
 const printKOMessage = (err) => {
 	console.dir(err)
 }
 
-getCollectors(collectorsDir)
-.then(files => {
-	files.forEach((collectorFile) => {
-		const collector = require(Path.resolve('./', collectorsDir, collectorFile))
-		collector(token)
-		.then((data) => {
-            const fileNameGroups = /([^\.]+)\./.exec(collectorFile)
-            if (!fileNameGroups) throw new Error(collectorFile + ' invalid file name')
-			return writeFile(data, fileNameGroups[1] || fileNameGroups[0])
-		})
-		.then(printOKMessage)
-		.catch(printKOMessage)
+const runCollectors = () => {
+    return getCollectors(collectorsDir)
+    .then((files)=>{
+        files.forEach((collectorFile) => {
+            const collector = require(path.resolve('./', collectorsDir, collectorFile))
+            const instance = new collector(token)
 
-	})
+            instance.run()
+            .then(printOKMessage)
+            .catch(printKOMessage)
+        })
+    })
+}
+
+cronJob.schedule('*/5 * * * *', () => {
+    console.log('Cron!')
+    runCollectors()
 })
-.catch((err) => {
-	console.dir(err)
-})
+
+
--- index2.js
+++
@@ -1,5 +0,0 @@
-const accessLogsCollector = require('./lib/collectors/slackTeamAccessLogs.js')
-
-const token = process.env.SLACK_API_TOKEN || '';
-
-new accessLogsCollector(token).run()
