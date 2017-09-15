138cae00f8a47e7adddc53fb5b953531e1e0eefd
Josep <josep.arpi@gmail.com>
2017-09-15 23:03:52 +0200

    Deployment config
    - Add docker file / docker compose file
    - Add logs

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-const accessLogsCollector = require('./lib/collectors/slackTeamAccessLogs.js')
 const path = require('path')
 const fs = require('fs')
 const cronJob = require('node-cron')
 const collectorsDir = path.resolve('.', 'lib/collectors')
 
-const token = process.env.SLACK_API_TOKEN || '';
-
+const token = process.env.SLACK_API_TOKEN || ''
+const dataPath = process.env.DATA_PATH || '.'
 
 const getCollectors = (dir) => {
-	return new Promise((resolve, reject) => {
-		return fs.readdir(dir, (err, files) => {
-			if (err) return reject(err)
-			resolve(files)
-		})
-	})
+  return new Promise((resolve, reject) => {
+    return fs.readdir(dir, (err, files) => {
+      if (err) return reject(err)
+      resolve(files)
+    })
+  })
 }
 
 const printOKMessage = (fileName) => {
-	console.dir('File updated')
+  console.dir('File updated')
 }
 
 const printKOMessage = (err) => {
-	console.dir(err)
+  console.dir(err)
 }
 
 const runCollectors = () => {
-    return getCollectors(collectorsDir)
-    .then((files)=>{
-        files.forEach((collectorFile) => {
-            const collector = require(path.resolve('./', collectorsDir, collectorFile))
-            const instance = new collector(token)
+  return getCollectors(collectorsDir)
+    .then((files) => {
+      files.forEach((collectorFile) => {
+        const collector = require(path.resolve('./', collectorsDir, collectorFile))
+        const instance = new collector(token, dataPath)
 
-            instance.run()
+        instance.run()
             .then(printOKMessage)
             .catch(printKOMessage)
-        })
+      })
     })
 }
 
 cronJob.schedule('*/5 * * * *', () => {
-    console.log('Cron!')
-    runCollectors()
+  console.log('Cron!')
+  runCollectors()
 })
-
-
